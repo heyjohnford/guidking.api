4598382b6401dab3dd04801ee940561a7fa0268a
johnamiahford <johnamiahford@gmail.com>
2018-04-10 23:02:31 -0400

    Move logic to controller file

--- src/router.js
+++ src/router.js
@@ -1,47 +1,7 @@
 const { Router } = require('express')
-const {
-  generateUuid,
-  retry,
-  responseTime,
-  errors,
-  handleErrors
-} = require('../helpers')
-const logger = require('../lib/logger')
-const { createGuid } = require('./repository')
+const { getGuids } = require('./contoller')
 
 const router = Router()
-
-function isAmountValid(amount) {
-  if (amount < 0) {
-    throw new errors.BadRequest('amount must not be a negative number')
-  }
-
-  if (amount > 1000) {
-    throw new errors.BadRequest('amount must be equal to or less than 1000')
-  }
-
-  if (Number.isNaN(amount)) {
-    throw new errors.BadRequest('amount must be a valid number')
-  }
-}
-
-async function getGuids(req, res, next) {
-  const { query } = req
-  const amount = Math.floor(Number(query.amount))
-
-  try {
-    isAmountValid(amount)
-    logger.info(`${amount} guid${amount !== 1 ? 's' : ''} requested`)
-
-    const result = await retry(() => generateUuid(amount), 2)
-
-    res.json(result)
-    createGuid(req, amount, responseTime(req.startAt))
-  } catch (err) {
-    logger.error(err.toString())
-    handleErrors(err, req, res, next)
-  }
-}
 
 router.get('/', getGuids)
 
