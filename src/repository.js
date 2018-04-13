d58489278d7d2d7b2a8a9a064675e4eda2752c92
johnamiahford <johnamiahford@gmail.com>
2018-04-12 20:31:52 -0400

    Flesh out repository methods

--- src/repository.js
+++ src/repository.js
@@ -1,22 +1,38 @@
-const { logger } = require('../lib/logger')
+const { config, connection, logger } = require('../lib')
+
+let cachedDb
 
 class Repository {
-  static async createGuid(payload, amount, responseTime) {
-    const { requestId, ip, url, headers } = payload
-    const guid = {
-      requestId,
-      ip,
-      url,
-      responseTime,
-      numberOfGuids: amount,
-      userAgent: headers['user-agent']
-    }
-
-    logger.info(`${requestId} guid entry request saved!`)
+  constructor() {
+    this.dbName = config.get('database:name')
   }
 
-  // async updateTotalGuidCount(amount) {
-  // }
+  async getDb() {
+    if (cachedDb) {
+      return cachedDb
+    }
+
+    const client = await connection()
+    const db = client.db(this.dbName)
+
+    logger.info(`using ${this.dbName} for mongodb context`)
+
+    cachedDb = db
+
+    return db
+  }
+
+  async getCollectionByName(collectionName) {
+    const db = await this.getDb()
+    return db.collection(collectionName)
+  }
+
+  async insertOne(payload) {
+    const { requestId } = payload
+    const collection = await this.getCollectionByName('transactions')
+
+    return collection.insertOne({ _id: requestId, ...payload })
+  }
 }
 
-module.exports = Repository
+module.exports = new Repository()
