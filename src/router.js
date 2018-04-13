4fb51df037b7b196cbe94c0122fc9605fc26a493
johnamiahford <johnamiahford@gmail.com>
2018-04-12 20:50:06 -0400

    Add health check endpoint

--- src/router.js
+++ src/router.js
@@ -4,5 +4,6 @@ const { getGuids } = require('./contoller')
 const router = Router()
 
 router.get('/', getGuids)
+router.get('/_healthcheck', (req, res) => res.json({ ok: true }))
 
 module.exports = router
