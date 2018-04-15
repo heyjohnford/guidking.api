d57e330b1ec18c6e2ceb21845bc84caa1d97c3ad
johnamiahford <johnamiahford@gmail.com>
2018-04-15 00:58:16 -0400

    Add collection, index, view creation from the app Clean up inserted payload

--- src/repository.js
+++ src/repository.js
@@ -2,6 +2,25 @@ const { config, connection, logger } = require('../lib')
 
 let cachedDb
 
+function removeRequestIdFromBody(payload) {
+  const { requestId, ...payloadOmitRequestId } = payload
+  return payloadOmitRequestId
+}
+
+async function setupCollections(db, collectionName) {
+  await db.createCollection(collectionName)
+  logger.info(`${collectionName} collection created`)
+
+  await db.createIndex(collectionName, 'numberOfGuids', { background: true })
+  logger.info(`index on ${collectionName}.numberOfGuids field created`)
+
+  await db.createCollection('total_guids', {
+    viewOn: collectionName,
+    pipeline: [{ $group: { _id: null, total: { $sum: '$numberOfGuids' } } }]
+  })
+  logger.info('total_guids view created')
+}
+
 class Repository {
   constructor() {
     this.dbName = config.get('database:name')
@@ -15,7 +34,14 @@ class Repository {
     const client = await connection()
     const db = client.db(this.dbName)
 
-    logger.info(`using ${this.dbName} for mongodb context`)
+    logger.info(`using ${this.dbName} for database context`)
+
+    try {
+      const collectionName = 'transactions'
+      await setupCollections(db, collectionName)
+    } catch (err) {
+      logger.error(err.toString())
+    }
 
     cachedDb = db
 
@@ -28,10 +54,14 @@ class Repository {
   }
 
   async insertOne(payload) {
-    const { requestId } = payload
     const collection = await this.getCollectionByName('transactions')
+    const body = removeRequestIdFromBody(payload)
 
-    return collection.insertOne({ _id: requestId, ...payload })
+    return collection.insertOne({
+      _id: payload.requestId,
+      ...body,
+      createdAt: new Date()
+    })
   }
 }
 
