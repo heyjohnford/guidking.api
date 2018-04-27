a71f630a47d4f768fabba5ac70e4bae612968981
johnamiahford <johnamiahford@gmail.com>
2018-04-26 23:20:51 -0400

    Add sockets

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const { config, logger } = require('../lib')
+const { config, logger, socketHandler } = require('../lib')
 const { requestMiddleware, handleErrors } = require('../helpers')
+const { serverCloseEvents } = require('../constants')
 const router = require('./router')
 
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
 const PORT = config.get('env:port')
 
 app.use(requestMiddleware)
@@ -16,6 +20,16 @@ app.use(router)
 
 app.use(handleErrors)
 
-app.listen(PORT, async () => {
+io.on('connection', socketHandler)
+
+serverCloseEvents.forEach((event) => {
+  process.on(event, (err) => {
+    logger.error(err.toString())
+    server.close()
+    process.exit(1)
+  })
+})
+
+server.listen(PORT, () => {
   logger.info(`🏁 ${config.get('appName')} listening on port ${PORT}`)
 })
