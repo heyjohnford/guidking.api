5c6b9a3a7b117e2cfaff824ba67fc211896e2936
John Ford <johnamiahford@gmail.com>
2019-01-11 10:50:12 -0500

    Add guid amount allowed to app config

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,8 @@
+const config = require('../lib/config')
+
 const serverCloseEvents = ['uncaughtException', 'SIGTERM']
 
 module.exports = {
-  serverCloseEvents
+  serverCloseEvents,
+  AMOUNT_OF_GUIDS_ALLOWED: config.get('app:numberOfGuidsAllowed')
 }
