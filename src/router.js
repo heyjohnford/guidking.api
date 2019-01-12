cd9b01fef1bf1ec6302c5ae66a0ae0794ccef470
John Ford <johnamiahford@gmail.com>
2019-01-11 19:43:51 -0500

    Add readiness endpoint

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ const router = Router()
 
 router.get('/', getGuids)
 router.get('/total', getTotalGuidsCount)
-router.get('/_healthcheck', (req, res) => res.status(200).json({ ok: true }))
+router.get('/healthcheck', (req, res) => res.status(200).json({ ok: true }))
+router.get('/readiness', (req, res) => res.status(200).json({ ok: true }))
 
 module.exports = router
